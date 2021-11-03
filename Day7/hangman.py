784e570e2e944d0a34e03814cd89bd6c0097a2f8
sammybarman <sambarta.raybarman@gmail.com>
2021-11-03 21:23:48 +0530

    Day 7

---
+++ Day7/hangman.py
@@ -0,0 +1,110 @@
+# create a hangman game
+from random_words import RandomWords
+# a python module that generates random words
+
+
+def hangman(word, word_l):
+
+    life = 6
+    i = 5
+    ans = ["_"]*len(word)
+    print("Welcome to Hangman!")
+    logo = ''' 
+    _                                             
+    | |                                            
+    | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+    | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+    | | | | (_| | | | | (_| | | | | | | (_| | | | |
+    |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |                      
+                    |___/    '''
+
+    print(logo)
+    print(emoji[6])
+    while life > 0 and "_" in ans:
+        print("You have {} lives left".format(life))
+        if ans == word:
+            print("You win!")
+            break
+        guess = input("Guess a letter: ").lower()
+        if guess in word:
+            print("You have guessed correctly!")
+            while guess in word:
+                index = word.index(guess)
+                ans[index] = guess
+                word[index] = "_"
+            print("".join(ans))
+        else:
+            life -= 1
+            print("You have guessed incorrectly!")
+            print(emoji[i])
+            i -= 1
+
+    if life == 0:
+        print("You lose!")
+        print("The word was {}".format(word_l))
+
+
+emoji = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+rw = RandomWords()
+word_l = rw.random_word().lower()
+word = []
+for i in word_l:
+    word.append(i)
+hangman(word, word_l)
