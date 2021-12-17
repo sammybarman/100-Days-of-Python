254848867ecc764fb65a684b72f91fa07e370ca8
sammybarman <sambarta.raybarman@gmail.com>
2021-12-17 12:38:12 +0530

    updates

---
+++ Day9/9-1.py
@@ -0,0 +1,7 @@
+'''
+you have access to a dictionary in the form student:scores
+change scores to grades according to the following rules:
+91-100 = outstanding
+81-90 = exceeds expectation
+71-80 = 
+'''
---
+++ Day9/silent_auction.py
@@ -0,0 +1,5 @@
+# create a program to silumalte a silent auction.
+'''
+Silent Auction - No one knows each other's bids.
+The bidder with the highest bid wins.
+'''
